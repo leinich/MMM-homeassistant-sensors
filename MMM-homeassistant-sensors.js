e96a10a5c882c996f8627f1ad52d4cd4ccd36b47
Florian Geckeler <floge77@gmail.com>
2020-01-28 21:31:05 +0100

    Add possibility to configure thresholds
    This change introduces a new config option called 'alertThreshold'.
    It can be configured per value and if configured and the measured value of
    the sensor exceeds threshold, the entry will blink.

--- MMM-homeassistant-sensors.js
+++ MMM-homeassistant-sensors.js
@@ -1,187 +1,204 @@
-'use strict';
+"use strict";
 
 Module.register("MMM-homeassistant-sensors", {
-	result: {},
-	defaults: {
-		prettyName: true,
-		stripName: true,
-		title: 'Home Assistant',
-		host: 'hassio.local',
-		port: '8321',
-		https: false, 
-		token: '',
-		apipassword: '',
-		updateInterval: 300000,
-		displaySymbol: true,
-		debuglogging: false,
-		values: []
-	},
+  result: {},
+  defaults: {
+    prettyName: true,
+    stripName: true,
+    title: "Home Assistant",
+    host: "hassio.local",
+    port: "8321",
+    https: false,
+    token: "",
+    apipassword: "",
+    updateInterval: 300000,
+    displaySymbol: true,
+    debuglogging: false,
+    values: []
+  },
 
-	getStyles: function () {
-		return ["modules/MMM-homeassistant-sensors/MaterialDesign-Webfont-master/css/materialdesignicons.min.css"];
-	},
+  getStyles: function() {
+    return [
+      "modules/MMM-homeassistant-sensors/MaterialDesign-Webfont-master/css/materialdesignicons.min.css",
+      "modules/MMM-homeassistant-sensors/hassio.css"
+    ];
+  },
 
-	start: function () {
-		this.getStats();
-		this.scheduleUpdate();
-	},
-	isEmpty: function (obj) {
-		for (var key in obj) {
-			if (obj.hasOwnProperty(key)) {
-				return false;
-			}
-		}
-		return true;
-	},
-	getDom: function () {
-		var wrapper = document.createElement("ticker");
-		wrapper.className = 'dimmed small';
-		var data = this.result;
-		var statElement = document.createElement("header");
-		var title = this.config.title;
-		statElement.innerHTML = title;
-		wrapper.appendChild(statElement);
+  start: function() {
+    this.getStats();
+    this.scheduleUpdate();
+  },
+  isEmpty: function(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  },
+  getDom: function() {
+    var wrapper = document.createElement("ticker");
+    wrapper.className = "dimmed small";
+    var data = this.result;
+    var statElement = document.createElement("header");
+    var title = this.config.title;
+    statElement.innerHTML = title;
+    wrapper.appendChild(statElement);
 
-		if (data && !this.isEmpty(data)) {
-			var tableElement = document.createElement("table");
-			var values = this.config.values;
-			if (values.length > 0) {
-				for (var i = 0; i < values.length; i++) {
-					var icons = values[i].icons[0];
-					var sensor = values[i].sensor;
-					var val = this.getValue(data, sensor);
-					var name = this.getName(data, sensor);
-					var unit = this.getUnit(data, sensor);
-					if (val) {
-						tableElement.appendChild(this.addValue(name, val, unit, icons));
-					}
-				}
-			} else {
-				for (var key in data) {
-					if (data.hasOwnProperty(key)) {
-						tableElement.appendChild(this.addValue(key, data[key], "", ""));
-					}
-				}
-			}
-			wrapper.appendChild(tableElement);
-		} else {
-			var error = document.createElement("span");
-			error.innerHTML = "Error fetching stats.";
-			wrapper.appendChild(error);
-		}
-		return wrapper;
-	},
-	getValue: function (data, value) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].entity_id == value) {
-				return data[i].state;
-			}
-		}
-		return null;
-	},
-	getUnit: function (data, value) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].entity_id == value) {
-				if (typeof data[i].attributes.unit_of_measurement !== "undefined") {
-					return data[i].attributes.unit_of_measurement;
-				}
-				return "";
-			}
-		}
-		return "";
-	},
-	getName: function (data, value) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].entity_id == value) {
-				return data[i].attributes.friendly_name;
-			}
-		}
-		return null;
-	},
-	addValue: function (name, value, unit, icons) {
-		var newrow,
-		newText,
-		newCell;
-		newrow = document.createElement("tr");
-		if (this.config.stripName) {
-			var split = name.split(".");
-			name = split[split.length - 1];
-		}
-		if (this.config.prettyName) {
-			name = name.replace(/([A-Z])/g, function ($1) {
-					return "_" + $1.toLowerCase();
-				});
-			name = name.split("_").join(" ");
-			name = name.replace(/\w\S*/g, function (txt) {
-					return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-				});
-		}
-		// icons
-		newCell = newrow.insertCell(0);
-		newCell.className = "align-left";
-		if (this.config.displaySymbol) {
-			if (typeof icons === "object") {
-				var iconsinline;
-				//Change icons based on HA status
-				if (value == "on" && typeof icons.state_on === "string") {
-					iconsinline = document.createElement("i");
-					iconsinline.className = "mdi mdi-" + icons.state_on;
-					newCell.appendChild(iconsinline);
-				} else if (value == "off" && typeof icons.state_off === "string") {
-					iconsinline = document.createElement("i");
-					iconsinline.className = "mdi mdi-" + icons.state_off;
-					newCell.appendChild(iconsinline);
-				} else if (value == "open" && typeof icons.state_open === "string") {
-					iconsinline = document.createElement("i");
-					iconsinline.className = "mdi mdi-" + icons.state_open;
-					newCell.appendChild(iconsinline);
-				} else if (value == "closed" && typeof icons.state_closed === "string") {
-					iconsinline = document.createElement("i");
-					iconsinline.className = "mdi mdi-" + icons.state_closed;
-					newCell.appendChild(iconsinline);
-				} else {
-					if (typeof icons.default === "string") {
-						iconsinline = document.createElement("i");
-						iconsinline.className = "mdi mdi-" + icons.default;
-						newCell.appendChild(iconsinline);
-					}
-				}
-			}
-		}
-		// Name
-		newCell = newrow.insertCell(1);
-		newText = document.createTextNode(name);
-		newCell.appendChild(newText);
-		// Value
-		newCell = newrow.insertCell(2);
-		newCell.className = "align-right"
-			newText = document.createTextNode(value);
-		newCell.appendChild(newText);
-		// Unit
-		newCell = newrow.insertCell(3);
-		newCell.className = "align-left"
-			newText = document.createTextNode(unit);
-		newCell.appendChild(newText);
-		return newrow;
-	},
-	scheduleUpdate: function (delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		var self = this;
-		setInterval(function () {
-			self.getStats();
-		}, nextLoad);
-	},
-	getStats: function () {
-		this.sendSocketNotification('GET_STATS', this.config);
-	},
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "STATS_RESULT") {
-			this.result = payload;
-			var fade = 500;
-			this.updateDom(fade);
-		}
-	},
+    if (data && !this.isEmpty(data)) {
+      var tableElement = document.createElement("table");
+      var values = this.config.values;
+      if (values.length > 0) {
+        for (var i = 0; i < values.length; i++) {
+          var icons = values[i].icons[0];
+          var sensor = values[i].sensor;
+          var val = this.getValue(data, sensor);
+          var name = this.getName(data, sensor);
+          var unit = this.getUnit(data, sensor);
+          var alertThreshold = values[i].alertThreshold;
+          if (val) {
+            tableElement.appendChild(
+              this.addValue(name, val, unit, icons, alertThreshold)
+            );
+          }
+        }
+      } else {
+        for (var key in data) {
+          if (data.hasOwnProperty(key)) {
+            tableElement.appendChild(
+              this.addValue(key, data[key], "", "", false)
+            );
+          }
+        }
+      }
+      wrapper.appendChild(tableElement);
+    } else {
+      var error = document.createElement("span");
+      error.innerHTML = "Error fetching stats.";
+      wrapper.appendChild(error);
+    }
+    return wrapper;
+  },
+  getValue: function(data, value) {
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].entity_id == value) {
+        return data[i].state;
+      }
+    }
+    return null;
+  },
+  getUnit: function(data, value) {
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].entity_id == value) {
+        if (typeof data[i].attributes.unit_of_measurement !== "undefined") {
+          return data[i].attributes.unit_of_measurement;
+        }
+        return "";
+      }
+    }
+    return "";
+  },
+  getName: function(data, value) {
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].entity_id == value) {
+        return data[i].attributes.friendly_name;
+      }
+    }
+    return null;
+  },
+  addValue: function(name, value, unit, icons, alertThreshold) {
+    var newrow, newText, newCell;
+    newrow = document.createElement("tr");
+    if (!isNaN(alertThreshold)) {
+      console.log("alertThreshold is a number");
+      if (alertThreshold < value) {
+        console.log("Threshold exceeded - blink it");
+        newrow.className += "blink";
+      }
+    }
+    if (this.config.stripName) {
+      var split = name.split(".");
+      name = split[split.length - 1];
+    }
+    if (this.config.prettyName) {
+      name = name.replace(/([A-Z])/g, function($1) {
+        return "_" + $1.toLowerCase();
+      });
+      name = name.split("_").join(" ");
+      name = name.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+    }
+    // icons
+    newCell = newrow.insertCell(0);
+    newCell.className = "align-left";
+    if (this.config.displaySymbol) {
+      if (typeof icons === "object") {
+        var iconsinline;
+        //Change icons based on HA status
+        if (value == "on" && typeof icons.state_on === "string") {
+          iconsinline = document.createElement("i");
+          iconsinline.className = "mdi mdi-" + icons.state_on;
+          newCell.appendChild(iconsinline);
+        } else if (value == "off" && typeof icons.state_off === "string") {
+          iconsinline = document.createElement("i");
+          iconsinline.className = "mdi mdi-" + icons.state_off;
+          newCell.appendChild(iconsinline);
+        } else if (value == "open" && typeof icons.state_open === "string") {
+          iconsinline = document.createElement("i");
+          iconsinline.className = "mdi mdi-" + icons.state_open;
+          newCell.appendChild(iconsinline);
+        } else if (
+          value == "closed" &&
+          typeof icons.state_closed === "string"
+        ) {
+          iconsinline = document.createElement("i");
+          iconsinline.className = "mdi mdi-" + icons.state_closed;
+          newCell.appendChild(iconsinline);
+        } else {
+          if (typeof icons.default === "string") {
+            iconsinline = document.createElement("i");
+            iconsinline.className = "mdi mdi-" + icons.default;
+            newCell.appendChild(iconsinline);
+          }
+        }
+      }
+    }
+    // Name
+    newCell = newrow.insertCell(1);
+    newText = document.createTextNode(name);
+    newCell.appendChild(newText);
+    // Value
+    newCell = newrow.insertCell(2);
+    newCell.className = "align-right";
+    newText = document.createTextNode(value);
+    newCell.appendChild(newText);
+    // Unit
+    newCell = newrow.insertCell(3);
+    newCell.className = "align-left";
+    newText = document.createTextNode(unit);
+    newCell.appendChild(newText);
+
+    return newrow;
+  },
+  scheduleUpdate: function(delay) {
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+    var self = this;
+    setInterval(function() {
+      self.getStats();
+    }, nextLoad);
+  },
+  getStats: function() {
+    this.sendSocketNotification("GET_STATS", this.config);
+  },
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "STATS_RESULT") {
+      this.result = payload;
+      var fade = 500;
+      this.updateDom(fade);
+    }
+  }
 });
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # MMM-homeassistant-sensors
-This a module for the [MagicMirror](https://github.com/MichMich/MagicMirror/tree/develop). 
+
+This a module for the [MagicMirror](https://github.com/MichMich/MagicMirror/tree/develop).
 It can display information from [Home Assistant](https://home-assistant.io/) using the home assistant REST API.
 
 ## Installation
+
 Navigate into your MagicMirror's `modules` folder and clone this repository:  
 `cd ~/MagicMirror/modules && git clone https://github.com/leinich/MMM-homeassistant-sensors.git`
 
@@ -10,41 +12,46 @@ If you want to use icons for the sensors download the `MaterialDesignIcons` webf
 `cd ~/MagicMirror/modules/MMM-homeassistant-sensors && wget https://github.com/Templarian/MaterialDesign-Webfont/archive/master.zip && unzip master.zip`
 
 ## Configuration
+
 It is very simple to set up this module, a sample configuration looks like this:
 
 ## Configuration Options
 
-| Option               | Description |
-| -------------------- | ----------- |
-| `prettyName`         | Pretty print the name of each JSON key (remove camelCase and underscores). <br><br> **Default:** `true` |
-| `stripName`          | Removes all keys before the printed key. <br><br>**Example:** `a.b.c` will print `c`.<br> **Default:** `true` |
-| `title`              | Title to display at the top of the module. <br><br> **Default:** `Home Assistant` |
-| `host`               | The hostname or ip adress of the home assistant instance. <br><br> **Default:** `REQUIRED hassio.local` |
-| `port`               | port of homeassistant e.g. 443 for SSL. <br><br> **Default:** `8321` |
-| `https`              | is SSL enabled on home assistant (true/false) <br><br> **Default:** `REQUIRED false` |
-| `token`              | The long lived token. <br><br> **Default:** `REQUIRED` |
-| `apipassword`        | Deprecated API password. <br><br> **Default:** `` |
-| `updateInterval`     | The time between updates (In milliseconds). <br><br> **Default:** `300000 (5 minutes)` |
-| `selfsigned`         | allows self signed certificates/ less secure (true/false). <br><br> **Default:** `false` |
-| `debuglogging`       | Enable logging into /home/pi/.pm2/logs/mm-error.log (true/false). <br><br> **Default:** `false` |
-| `values`             | Specify specific values from the json feed to only show what you need (entity_id). |
+| Option           | Description                                                                                                   |
+| ---------------- | ------------------------------------------------------------------------------------------------------------- |
+| `prettyName`     | Pretty print the name of each JSON key (remove camelCase and underscores). <br><br> **Default:** `true`       |
+| `stripName`      | Removes all keys before the printed key. <br><br>**Example:** `a.b.c` will print `c`.<br> **Default:** `true` |
+| `title`          | Title to display at the top of the module. <br><br> **Default:** `Home Assistant`                             |
+| `host`           | The hostname or ip adress of the home assistant instance. <br><br> **Default:** `REQUIRED hassio.local`       |
+| `port`           | port of homeassistant e.g. 443 for SSL. <br><br> **Default:** `8321`                                          |
+| `https`          | is SSL enabled on home assistant (true/false) <br><br> **Default:** `REQUIRED false`                          |
+| `token`          | The long lived token. <br><br> **Default:** `REQUIRED`                                                        |
+| `apipassword`    | Deprecated API password. <br><br> **Default:** ``                                                             |
+| `updateInterval` | The time between updates (In milliseconds). <br><br> **Default:** `300000 (5 minutes)`                        |
+| `selfsigned`     | allows self signed certificates/ less secure (true/false). <br><br> **Default:** `false`                      |
+| `debuglogging`   | Enable logging into /home/pi/.pm2/logs/mm-error.log (true/false). <br><br> **Default:** `false`               |
+| `values`         | Specify specific values from the json feed to only show what you need (entity_id).                            |
 
 ## values option
-| Option               | Description |
-| -------------------- | ----------- |
-| `sensor`             | `entity_id` from Home Assistant. Please have a look at the states pages for the unique `entity_id` of your sensor |
-| `icons`              | Icons object for the on/off status of sensor. see: [MaterialDesignIcons](https://materialdesignicons.com/) |
+
+| Option           | Description                                                                                                                                |
+| ---------------- | ------------------------------------------------------------------------------------------------------------------------------------------ |
+| `sensor`         | `entity_id` from Home Assistant. Please have a look at the states pages for the unique `entity_id` of your sensor                          |
+| `alertThreshold` | As soon as the measured value of the sensor exceeds the configured threshold, the entry will begin to 'blink'. <br><br> **Default:** `off` |
+| `icons`          | Icons object for the on/off status of sensor. see: [MaterialDesignIcons](https://materialdesignicons.com/)                                 |
 
 ## icons option
-| Option               | Description |
-| -------------------- | ----------- |
-| `default`            | Default icon of the sensor. In case there is no on/off status, like processor use. |
-| `state_on`           | On status icon of the sensor |
-| `state_off`          | Off status icon of the sensor |
-| `state_open`         | Open status icon of the sensor |
-| `state_closed`       | Closed status icon of the sensor |
+
+| Option         | Description                                                                        |
+| -------------- | ---------------------------------------------------------------------------------- |
+| `default`      | Default icon of the sensor. In case there is no on/off status, like processor use. |
+| `state_on`     | On status icon of the sensor                                                       |
+| `state_off`    | Off status icon of the sensor                                                      |
+| `state_open`   | Open status icon of the sensor                                                     |
+| `state_closed` | Closed status icon of the sensor                                                   |
 
 Here is an example of an entry in `config.js`
+
 ```
 modules: [{
 		module: 'MMM-homeassistant-sensors',
@@ -59,6 +66,7 @@ modules: [{
 			debuglogging: false,
 			values: [{
 					sensor: "sensor.processor_use",
+					alertThreshold: 50,
 					icons: [{
 							"default": "chip"
 						}
@@ -84,10 +92,12 @@ modules: [{
 	}
 ]
 ```
+
 **Result** example:
 
 ![Alt text](https://image.ibb.co/b8edjx/dynamic_icons.png "dynamic icons example")
 
 ## Special Thanks
+
 - [Michael Teeuw](https://github.com/MichMich) for creating the awesome [MagicMirror2](https://github.com/MichMich/MagicMirror/tree/develop) project that made this module possible.
 - [tkoeberl](https://github.com/tkoeberl) for creating the initial module that I used as guidance in creating this module.
