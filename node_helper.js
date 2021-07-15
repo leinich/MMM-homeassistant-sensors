b49bf00d798eb57fa306515ac2f11f7a98b72c2d
lolouk44 <laurent.mahe@gmail.com>
2021-07-15 16:33:40 +0100

    Removed deprecated Request and replaced with Got

--- node_helper.js
+++ node_helper.js
@@ -1,32 +1,46 @@
 var NodeHelper = require('node_helper');
-var request = require('request');
+const got = require('got');
 
 module.exports = NodeHelper.create({
   start: function () {
     if(config.debuglogging) { console.log('MMM-homeassistant-sensors helper started...') };
   },
   getStats: function (config) {
-      var self = this;
-      var url = self.buildUrl(config);
-      var get_options = {
-          url: url,
-          json: true
-        };
-      if(config.token.length > 1) {
-          if(config.debuglogging) {console.error('MMM-homeassistant-sensors: Adding token', config.token)}
-          get_options.headers = { 'Authorization' : 'Bearer ' + config.token };
-        }
-      request(get_options, function (error, response, body) {
-          if(config.debuglogging) {
-            console.error('MMM-homeassistant-sensors ERROR:', error);
-            console.error('MMM-homeassistant-sensors statusCode:', response.statusCode);
-            console.error('MMM-homeassistant-sensors Body:', body);
-          }
-          if (!error && response.statusCode == 200) {
-            if(config.debuglogging) { console.log('MMM-homeassistant-sensors response successfull. calling STATS_RESULT') };
-            self.sendSocketNotification('STATS_RESULT', body);
-          } 
-      });
+	var self = this;
+	var url = self.buildUrl(config);
+	var instance = got.extend({
+	  hooks: {
+		  beforeRequest: [
+			  options => {
+				  if (!options.context || !options.context.token) {
+					throw new Error('Long Lived Token required');
+				  }
+				  json: true;
+				  options.headers = {'Authorization' : 'Bearer ' + options.context.token}
+			  }
+		  ]
+	  }
+	});
+	(async () => {
+		var context = {
+			token: config.token
+		};
+
+		try {
+			var response = await instance(url, {context});
+			if (response.statusCode == 200) {
+				if(config.debuglogging) { console.log('MMM-homeassistant-sensors response successfull. calling STATS_RESULT') };
+				self.sendSocketNotification('STATS_RESULT', JSON.parse(response.body));
+			}
+			if(config.debuglogging) {
+				console.log('MMM-homeassistant-sensors Body:', response.body);
+				console.log('MMM-homeassistant-sensors statusCode:', response.statusCode);
+			}
+		} catch (error) {
+			console.log('MMM-homeassistant-sensors - Connection Failed: ' + error.response.body);
+		}
+	})();
+
   },
   buildUrl: function(config) {
     var url = config.host;
@@ -34,9 +48,6 @@ module.exports = NodeHelper.create({
       url = url + ':' + config.port;
     }
     url = url + '/api/states'
-    if (config.apipassword.length > 1) {
-      url = url + '?api_password=' + config.apipassword;
-    }
     if (config.https) {
       url = 'https://' + url;
     } else {
